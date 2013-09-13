de5d2e120cee26f454a48283e78cbcbc5b365b63
Rob Fletcher <rob@terraflubb.com>
2013-09-13 17:51:26 -0400

    Hard code the stuff to pair the controller

--- dozen.rb
+++ dozen.rb
@@ -27,12 +27,15 @@ class Dozen
       device.open do |handle|
         master = query_for_bluetooth_master(handle)
         puts "Sixaxis #{index} master is: #{master.each_byte.map { |b| b.to_s(16) }.join(':')}"
+        set_bluetooth_master(handle)
+        master = query_for_bluetooth_master(handle)
+        puts "Sixaxis #{index} master is: #{master.each_byte.map { |b| b.to_s(16) }.join(':')}"
       end
     end
   end
 
   private
-  
+
   def find_sixaxis_devices
     @usb.devices(:idVendor => 0x054c, :idProduct => 0x0268)
   end
@@ -40,11 +43,24 @@ class Dozen
   def query_for_bluetooth_master(handle)
     handle.control_transfer(
       :bmRequestType => LIBUSB::REQUEST_TYPE_CLASS | LIBUSB::ENDPOINT_IN | LIBUSB::RECIPIENT_INTERFACE,
-      :bRequest => 0x01, 
+      :bRequest => 0x01,
       :wValue => 0x03f5,
       :wIndex => 0x0000,
       :dataIn => 8,
       :timeout => 5000)[2,6]
   end
-end
 
+  def set_bluetooth_master(handle)
+    # starts with 0x01, 0x00, then your bluetooth address
+    bluetooth_addy = [0x01, 0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
+
+    msg = bluetooth_addy.pack('c*')
+    handle.control_transfer(
+      :bmRequestType => LIBUSB::REQUEST_TYPE_CLASS | LIBUSB::ENDPOINT_OUT | LIBUSB::RECIPIENT_INTERFACE,
+      :bRequest => 0x09,
+      :wValue => 0x03f5,
+      :wIndex => 0x0000,
+      :dataOut => msg,
+      :timeout => 5000)
+  end
+end
